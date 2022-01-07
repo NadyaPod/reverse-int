3c429514d95537d7d5720c2d1ccf9d16f9da5a27
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-01-07 23:44:37 +0700

    feat: implement reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+function reverse (number) {
+  const strNumber = number.toString(); 
+  let reversed = "";
+
+  for (let i = 0; i < strNumber.length; i++) {
+    let rIndex = (strNumber.length - i) - 1;
+
+    if (strNumber[rIndex] !== "-") {
+  	  reversed += strNumber[rIndex];
+    }
+  }
+  return Number(reversed);
 }
+
+module.exports = reverse;
